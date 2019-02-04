b4d5e32d08d3e7622f8170f3da203858cbccd394
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2019-02-04 19:11:23 +1000

    Add function to calculate

--- src/contracts/ERC20.sol
+++ src/contracts/ERC20.sol
@@ -2,25 +2,38 @@ pragma solidity ^0.5.2;
 
 contract ERC20 {
 
+    uint256 public _rate; //to 4 decimals
+
     struct Payment {
         uint amount;
-        uint timestamp;
+        uint blockNumber;
     }
 
     mapping (address => Payment[]) private _payments;
 
     constructor () public {
-        _payments[msg.sender].push(Payment(10000, now));
+        _payments[msg.sender].push(Payment(10000, block.number));
+        _rate = 1000;
     }
 
     function totalSupply() public view returns (uint) {
-
-
         return 0;
     }
 
-    function balanceOf(address who) public view returns (uint) {
+    function balanceOf(address who) public view returns (uint256) {
         Payment[] memory payments = _payments[who];
-        return payments.length;
+
+        uint256 total = 0;
+
+        for (uint i = 0; i < payments.length; i++) {
+            uint delta = block.number - payments[i].blockNumber;
+            total += delta * _rate;
+        }
+
+        return total;
     }
+
+    // function setRate(uint256 rate) public {
+    //     _rate = rate;
+    // }
 }
